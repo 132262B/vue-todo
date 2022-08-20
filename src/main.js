5dc599e6cc9fcbafb0b4b2087d26214866bba417
Igor <132262b@naver.com>
2022-08-20 15:16:33 +0900

    vuex 설치

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { store } from './store/store'
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(stroe)
   .mount('#app')
